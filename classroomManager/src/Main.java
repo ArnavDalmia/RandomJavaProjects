8515ed00a8abd7275eed373f58de1e8fdb49cb95
Arnav Dalmia <70185864+ArnavDalmia@users.noreply.github.com>
2022-11-22 00:30:18 -0500

    documentation

--- classroomManager/src/Main.java
+++ classroomManager/src/Main.java
@@ -1,46 +1,75 @@
-
+/*public class Main
+ * This class is the super class Main
+ * It operates the entire program
+ * 
+ * Purpose of program is to manage a 2 dimensional array or even called a table
+ */
 import java.util.Scanner;
 public class Main {
-
+	/* method createMatrix
+	 * This method creates an instance of Matrix, using user input
+	 * Parameters:
+	 * 		none
+	 * Return Value
+	 * 		Matrix variable that is initialized using the row and column variables, and possible the value variable if overload occurs
+	 * Local Variables:
+	 * 		row : int variable that represents the user inputted number of rows
+	 * 		column : int variable that represents the user inputted number of columns
+	 * 		value : int variable that represents the user inputted integer fill value
+	 * 		valid : boolean variable that is used in looping and veryfing if the user input is a yes or no
+	 *		answer  : String variable containing the user input in relation to a yes or no question
+	 * 		temp : Matrix variable that is initialized using user input
+	 */
 	public static Matrix createMatrix(){
-		Scanner userInput = new Scanner(System.in);
-		System.out.println("Create Your Matrix\n");
+		Scanner userInput = new Scanner(System.in); // initializes Random object
+		System.out.println("Create Your Matrix\n"); //message of initialization
 
-		int row;
+		int row; // initializes variables row, column, value
 		int column;
 		int value;
 
-		row = Utils.obtainIntegerInputLow("Please enter number of rows: ", 1);
+		row = Utils.obtainIntegerInputLow("Please enter number of rows: ", 1); //assigns the row and column to user input that has been error checked using Utils methods 
 		column = Utils.obtainIntegerInputLow("Please enter number of columns: ", 1);
 		
-		boolean valid;
-		valid  = false;
+		boolean valid; // initializes variable valid
+		valid  = false; // assigns to false for looping
 		while(!valid){	
 			String answer;
-			System.out.println("Would you like to set the values of the matrix right now to ONE specific number?(YES/NO)");
+			System.out.println("Would you like to set the values of the matrix right now to ONE specific number?(YES/NO)"); //asks user for yes or no to if they want to choose a value
 			System.out.println("If NO then values will be set to random numbers, UNTIL CHANGED.");
 
-			answer = userInput.nextLine();																							
+			answer = userInput.nextLine(); // recieves the user input and converts to all caps																					
 			answer = answer.toUpperCase();
 																	
-			if(answer.equals("YES"))
-				valid = true;													
-			else if (answer.equals("NO"))											
-				break;																		
-			else 
-				System.out.println("Invalid Input. Please input either YES or NO.");		
+			if(answer.equals("YES")) //checks if it is YES
+				valid = true;					//changes valid to break loop								
+			else if (answer.equals("NO"))									//checks if it is NO		
+				break;														//if so breaks loop while maintaining that valid is false
+			else //if the user doesnt input either  a yes or no
+				System.out.println("Invalid Input. Please input either YES or NO.");		//error message
 		}
 
-		if(valid){
-			value = Utils.obtainIntegerInputLowHigh("Please enter the specific value: ",10,99);
-			return new Matrix(row, column, value);
+		if(valid){ //if the user selected yes
+			value = Utils.obtainIntegerInputLowHigh("Please enter the specific value: ",10,99); //asks user for input regarding fill value
+			return new Matrix(row, column, value); //returns new Matrix object using overloaded contructor
 		}
-		Matrix temp = new Matrix(row, column);
-		temp.setRandomValues(10,100);
-		return temp;
+		//if the previous code from line 52 isnt run :
+		Matrix temp = new Matrix(row, column); //initializes a temp Matrix
+		temp.setRandomValues(10,100); //assigns it to random numbers
+		return temp;//returns temp
 	} 
+	
+	/* method menu
+	 * This method optains the user input to the main menu
+	 * Parameters:
+	 * 		none
+	 * Return Value
+	 * 		int variable containing the user inputted menu option
+	 * Local Variables:
+	 * 		none
+	 */
 	public static int menu(){
-		System.out.println("1.	Display matrix");		
+		System.out.println("1.	Display matrix");		//displays menu options
 		System.out.println("2.	Print specific Row");	
 		System.out.println("3.	Print specific Column");							
 		System.out.println("4.	Print the transpose(opposite) of matrix");
@@ -52,121 +81,144 @@ public class Main {
 		System.out.println("10.	RESET MATRIX");
 		System.out.println("11.	Check if matrix is equal to another materix");
 		System.out.println("12.	Quit");
-		return Utils.obtainIntegerInputLowHigh("",1,12);				     
+		return Utils.obtainIntegerInputLowHigh("",1,12);				 //returns option number, and verefies it using the Utils class    
 	}
 
-
+	/* method main
+	 * This method operates the entire program
+	 * Parameters:
+	 * 		none
+	 * Return Value
+	 * 		none
+	 * Local Variables:
+	 * 		initialMatrix : Matrix variable that contains the Matrix object
+	 * 		choice : int variable contains the return of menu method
+	 * 
+	 * 		max : int variable that represents the max number of rows
+	 * 		row : int variable that contains the user input regarding the specific row
+	 * 		specificRow : int [] variable that contains the specific row based on variable row
+	 * 
+	 * 		max : int variable that represents the max number of columns 
+	 * 		column : int variable that contains the user input regarding the specific column
+	 * 		specificColumn : int [] variable that contains the specific column based on variable column
+	 * 
+	 * 		transpose : Matrix variable that contains the transpose of initialMatrix
+	 * 
+	 * 		low : int variable that represents the low bound for the random range of numbers
+	 * 		high : int variable that represents the high bound for the random range of numbers
+	 * 		
+	 * 		value : int variable that contains the specific value for the integer fill of initalMatrix
+	 * 
+	 * 		secondMatrix : Matrix variable that contains another instance of Matrix
+	 * 		
+	 */
 	public static void main(String[] args) {
-		Matrix initialMatrix;
-		System.out.println("Welcome to the Matrix creation program.");
-		initialMatrix = createMatrix();
-		System.out.println("Matrix has been made.\n\n");
+		Matrix initialMatrix; // initializes initialMatrix
+		System.out.println("Welcome to the Matrix creation program."); //welcome message
+		initialMatrix = createMatrix(); //calls createMatrix method and contains return in variable initialMatrix
+		System.out.println("Matrix has been made.\n\n"); // success message of creation of initalMatrix
 		
-		while(true){
-			int choice;
-			choice = menu();
+		while(true){ //commencing loop for menu options
+			int choice; //initalize choice
+			choice = menu(); // contains return of menu method within choice variable
 			
-			if (choice == 1){
-				initialMatrix.displayArray();
-			}
-			
-			else if(choice == 2){
-				int max;
-				max = initialMatrix.getRowCount();
+			if (choice == 1){ // 1.	Display matrix
+				initialMatrix.displayArray(); // calls displayArray method using intialMatrix variable. Displays initialMatrix in proper method
+			}	
+			else if(choice == 2){ // 2.	Print specific Row
+				int max; //initialize max
+				max = initialMatrix.getRowCount(); //max contains the number of rows using getRowCount method
 
-				int row;
-				row = Utils.obtainIntegerInputLowHigh("Enter The row number that you would like printed: ", 1, max);
-				int [] specificRow;
-				specificRow = initialMatrix.getRow(row-1);
-				//display portion needs to be done
-				System.out.println("\n"+initialMatrix.returnDashes(initialMatrix.getColumnCount() * 4));
+				int row; //initialize row
+				row = Utils.obtainIntegerInputLowHigh("Enter The row number that you would like printed: ", 1, max); //row contains the user inputted specific row to be printed
+				int [] specificRow; //initialize specificRow
+				specificRow = initialMatrix.getRow(row-1); //specificRow is set to the row at position row-1 using getRow method
+				System.out.println("\n"+initialMatrix.returnDashes(initialMatrix.getColumnCount() * 4));// dashes that are placed above the row for organization using returnDashes method
 				
-				for (int j=0; j<specificRow.length ; j++)  {
-					System.out.print("|");
+				for (int j=0; j<specificRow.length ; j++)  {//loops through specificRow
+					System.out.print("|");//seperates members of row using '|'
 					System.out.print(specificRow[j]);
 					System.out.print("|");
 				} //end for j
 				
-				System.out.println("\n"+initialMatrix.returnDashes(initialMatrix.getColumnCount() * 4));
+				System.out.println("\n"+initialMatrix.returnDashes(initialMatrix.getColumnCount() * 4)); // dashes that are placed under the row for organization using returnDashes method
 
 			}
-			else if(choice == 3){
-				int max;
-				max = initialMatrix.getColumnCount();
+			else if(choice == 3){ //3.	Print specific Column
+				int max; //initialize max
+				max = initialMatrix.getColumnCount(); //max contains the number of columns using getColumnCount method
 
-				int row;
-				row = Utils.obtainIntegerInputLowHigh("Enter The column number that you would like printed: ", 1, max);
-				int [] specificColumn;
-				specificColumn = initialMatrix.getColumn(row-1);
-				//display portion needs to be done
-				System.out.println("\n"+"----");
-				for (int j=0; j<specificColumn.length ; j++)  {
-					System.out.print("|");
+				int column; // initialize variable column
+				column = Utils.obtainIntegerInputLowHigh("Enter The column number that you would like printed: ", 1, max); //column now contains the user input of which colum the user wants to output
+				int [] specificColumn; // initialize specificColumn
+				specificColumn = initialMatrix.getColumn(column-1); // sets specificColumn to the column at position column-1 using getColumn method
+				System.out.println("\n"+"----"); // dashes that are placed above the column for organization
+				for (int j=0; j<specificColumn.length ; j++)  { //loops through column
+					System.out.print("|"); //seperates members of column using '|'
 					System.out.print(specificColumn[j]);
 					System.out.print("|");
-					System.out.println("\n"+"----");
+					System.out.println("\n"+"----"); //seperates between rows using dashes
 				} //end for j
 
 			}
-			else if(choice == 4){
-				Matrix transpose;
-				transpose = initialMatrix.getTranspose();
-				transpose.displayArray();
+			else if(choice == 4){ //4.	Print the transpose(opposite) of matrix
+				Matrix transpose; // initialize transpose
+				transpose = initialMatrix.getTranspose(); //obtain return from getTranspose and store within transpose
+				transpose.displayArray(); // displays the transpose
 			}
-			else if(choice == 5){
-				int low;
-				int high;
-				low = Utils.obtainIntegerInputLowHigh("What would you like to be lowbound of the random numbers: ",0,99);
+			else if(choice == 5){// 5.	Set random values of matrix
+				int low; //initialize variable low
+				int high;//initialize variable high
+				low = Utils.obtainIntegerInputLowHigh("What would you like to be lowbound of the random numbers: ",0,99); //sets low and high to the user input of what the user desires the low and high bound of the random range to be
 				high = Utils.obtainIntegerInputLowHigh("What would you like to be highbound of the random numbers: ",0,99);
-				if (high < low){
+				if (high < low){ //checks if the low bound is greater than the high bound, which is not possible so an error message shows
 					System.out.println("Error. High bound must be greater than Low bound. Please Try Again.\n");
 				}
-				else{
-					initialMatrix.setRandomValues(low, high+1);
-					System.out.println("Successfully set values to be randomized between low and high ranges\n");
+				else{ // if the low and high bound are valid
+					initialMatrix.setRandomValues(low, high+1); //sets the values using setRandomValues method
+					System.out.println("Successfully set values to be randomized between low and high ranges\n"); //success message
 
 				}
 			}
-			else if(choice == 6){
-				int value;
-				value = Utils.obtainIntegerInputLowHigh("Enter specific value: ", 10, 99);
-				initialMatrix.setSpecificValue(value);
-				System.out.println("Matrix has been successfully changed to contain solely the value of "+value+".");
+			else if(choice == 6){//6.	Set specific values of matrix
+				int value; //initialize value
+				value = Utils.obtainIntegerInputLowHigh("Enter specific value: ", 10, 99); //obtains the value that the user wants as their value and contains it within value
+				initialMatrix.setSpecificValue(value); // calls setSpecificValue with value parameter and changes all the entries within initialMatrix to value
+				System.out.println("Matrix has been successfully changed to contain solely the value of "+value+"."); //success message
 			}
-			else if(choice == 7){
-				System.out.println("Matrix smallest value is "+initialMatrix.smallestEntry()+"\n");
+			else if(choice == 7){ // 7.	Print smallest entry
+				System.out.println("Matrix smallest value is "+initialMatrix.smallestEntry()+"\n"); //prints out the smallest entry within Matrix
+			}
+			else if(choice == 8){ // 8.	Print largest entry
+				System.out.println("Matrix largest value is "+initialMatrix.largestEntry()+"\n"); // prints the largest entry within Matrix
+			}
+			else if(choice == 9){ // 9.	Print average of entries
+				System.out.println("Matrix average of values is "+initialMatrix.averageOfEntries()+"\n"); //prints out message containing the average of all the entries in the Matrix
+			}
+			else if(choice == 10){ // 10.	RESET MATRIX
+				System.out.println("Matrix has been reset to empty"); //message of deletion of Matrix
+				System.out.println("Information is required:\n\n"); //tells user they require to re-enter info regarding the Matrix
+
+				initialMatrix = createMatrix(); //calls createMatrix to initialize the intialMatrix once again with different values
+				System.out.println("Matrix has been reset.\n\n"); //success message
 
 			}
-			else if(choice == 8){
-				System.out.println("Matrix largest value is "+initialMatrix.largestEntry()+"\n");
-			}
-			else if(choice == 9){
-				System.out.println("Matrix average of values is "+initialMatrix.averageOfEntries()+"\n");
-			}
-			else if(choice == 10){
-				System.out.println("Matrix has been reset to empty");
-				System.out.println("Information is required:\n\n");
+			else if(choice == 11){//11.	Check if matrix is equal to another materix
+				Matrix secondMatrix; // initialize secondMatrix
+				System.out.println("Creating the second matrix"); 
+				secondMatrix = createMatrix(); // assigning values to secondMatrix using createMatrix method
+				System.out.println("Created the second matrix.\n"); //success message
 
-				initialMatrix = createMatrix();
-				System.out.println("Matrix has been reset.\n\n");
+				secondMatrix.displayArray(); //displays secondMatrix to show user that it successfully was created
 
-			}
-			else if(choice == 11){
-				Matrix secondMatrix;
-				System.out.println("Creating the second matrix");
-				secondMatrix = createMatrix();
-				System.out.println("Created the second matrix.\n");
-
-				secondMatrix.displayArray();
-
-				if (initialMatrix.getEquals(secondMatrix))
+				if (initialMatrix.getEquals(secondMatrix)) // if both Matrix objects are the same, checked using the getEquals method on initialMatrix
 					System.out.println("\nThese two matrixes are the same!\n");
-				else
+				else // message if the two are not the same
 					System.out.println("\nThese two matrixes are not the same!\n");
 
 			}
-			else
-				break;
+			else // 12. quit
+				break; //breaks out of loops
 			
 		}//while loop 
 
--- classroomManager/src/Matrix.java
+++ classroomManager/src/Matrix.java
@@ -46,136 +46,267 @@ public class Matrix { //class start
 		setSpecificValue(integerFillValue); // sets the default values of the field variable matrix using the method setSpecificValue
 	}
 	
-	
+	/* method setRandomValues
+	 * This method sets random values within the matrix field variable 
+	 * Parameters:
+	 * 		min : int variable that represents the low bound of the random range
+     *      max : int variable that represents the high bound of the random range
+	 * Return Value
+	 * 		none
+	 * Local Variables:
+	 * 		rand : Random variable, used to generate random int
+	 */
 	public void setRandomValues(int min,int max) {
-		Random rand = new Random();
-		for (int i=0; i<matrix.length; i++) {
-			for (int j=0; j<matrix[0].length; j++) {
-				matrix[i][j] = rand.nextInt(max-min) + min;
+		Random rand = new Random(); // create instance of Random
+		for (int i=0; i<matrix.length; i++) { //loop in row of matrix
+			for (int j=0; j<matrix[0].length; j++) { //loop through columns of matrix
+				matrix[i][j] = rand.nextInt(max-min) + min; // sets a random value and used the rand variable 
 			}//end of j
 		}//end of i
 	}
+
+	/* method setSpecificValue
+	 * This method sets random values within the matrix field variable 
+	 * Parameters:
+	 * 		value : int variable that represents specific value that will fill in the entire matrix field variable
+	 * Return Value
+	 * 		none
+	 * Local Variables:
+	 * 		none
+	 */
 	public void setSpecificValue(int value) {
-		for (int i=0; i<matrix.length; i++) {
-			for (int j=0; j<matrix[0].length; j++) {
-				matrix[i][j] = value;
+		for (int i=0; i<matrix.length; i++) { // loops through rows of matrix
+			for (int j=0; j<matrix[0].length; j++) { // loops through columns of matrix
+				matrix[i][j] = value; // assigns every iteration of matrix to value parameter variable
 			}//end of j
 		}//end of i
 	}	
 	
+	/* method largestEntry
+	 * This method returns the largest value within matrix field variable
+	 * Parameters:
+	 * 		none
+	 * Return Value
+	 * 		highest : int variable that represents the highest value within matrix field variable
+	 * Local Variables:
+	 * 		highest : int variable that represents the highest value within matrix field variable
+	 */
 	public int largestEntry () {
-		int highest; 
-		highest = matrix[0][0];
-		for (int i=0; i<matrix.length; i++) {
+		int highest; //initializes highest
+		highest = matrix[0][0]; // assigns highets to first value of matrix
+		for (int i=0; i<matrix.length; i++) { //loops through matrix
 			for (int j=0; j<matrix[0].length; j++) {
-				if (matrix[i][j] > highest)
-					highest  = matrix[i][j];
+				if (matrix[i][j] > highest) //checks if teh current value is greater than highest
+					highest  = matrix[i][j]; // if it is greater than it re-assigns the value of highest
 				
 			}//end of j
 		}//end of i
-		return highest;
+		return highest; //returns the highest/largest value of matrix
 	}
+	/* method smallestEntry
+	 * This method returns the smallest value within matrix field variable
+	 * Parameters:
+	 * 		none
+	 * Return Value
+	 * 		lowest : int variable that represents the smallest value within matrix field variable
+	 * Local Variables:
+	 * 		lowest : int variable that represents the smallest value within matrix field variable
+	 */
 	public int smallestEntry () {
-		int lowest; 
-		lowest = matrix[0][0];
-		for (int i=0; i<matrix.length; i++) {
+		int lowest; //initializes lowest
+		lowest = matrix[0][0]; //assigns lowest to first value of matrix
+		for (int i=0; i<matrix.length; i++) { //loops through matrix
 			for (int j=0; j<matrix[0].length; j++) {
-				if (matrix[i][j] < lowest)
-					lowest  = matrix[i][j];
+				if (matrix[i][j] < lowest) //checks if current value is smaller than lowest
+					lowest  = matrix[i][j]; //if it is then it re-assigns the value of lowest to the current value
 				
 			}//end of j
 		}//end of i
-		return lowest;
+		return lowest; //returns the lowest of the entries in matrix
 	}
+
+
+	/* method averageOfEntries
+	 * This method returns the average of all the entries within matrix
+	 * Parameters:
+	 * 		none
+	 * Return Value
+	 * 		double variable that represents average of the entries
+	 * Local Variables:
+	 * 		number : int variable that represents the total sum of all the entries
+	 * 		counter : int variable that represents the number of entries within the matrix
+	 */
 	public double averageOfEntries () {
-		int number; 
+		int number;  //initializes number and counter
 		int counter;
-		number = 0;
+		number = 0; //sets both local variables to 0 initially, they will add to as the program continues
 		counter = 0;
 		
-		for (int i=0; i<matrix.length; i++) {
+		for (int i=0; i<matrix.length; i++) { //loops through matrix
 			for (int j=0; j<matrix[0].length; j++) {
-				number+=matrix[i][j];
-				counter+=1;
+				number+=matrix[i][j]; // adds each entries value to the variable number
+				counter+=1; // adds 1 for each entry in counter
 			}//end of j
 		}//end of i
-		return Utils.roundDouble((double)number/counter, 2);//round it
+		return Utils.roundDouble((double)number/counter, 2);//returns the average using the mean formula, and rounds the double to 2 decimal places, if neccessary
 	}
 	
+	/* method returnDashes
+	 * This method returns dashes for formatting the 2 dimensional array
+	 * Parameters:
+	 * 		count : int variable that represents number of dashes that will be printed
+	 * Return Value
+	 * 		temp : string variable that contains the dashes
+	 * Local Variables:
+	 *	 	temp : string variable that contains the dashes
 
+	 */
 	public static String returnDashes(int count) {
-		String temp = "";
-		for (int i=0; i<count;i++)
-			temp+="-";
-		return temp; 
+		String temp = ""; // initializes the temp variable
+		for (int i=0; i<count;i++) //loops through count
+			temp+="-"; //adds a dash for every iteration of count
+		return temp; //returns the filled in temp string
 	}
+	
+	/* method displayArray
+	 * This method returns a output for the display of matrix in a formatted fashion
+	 * Parameters:
+	 * 		none
+	 * Return Value
+	 * 		none
+	 * Local Variables:
+	 *	 	none
+
+	 */
 	public void displayArray() {
-		System.out.println("\n"+returnDashes(getColumnCount() * 4));
+		System.out.println("\n"+returnDashes(getColumnCount() * 4));//prints string line of dashes to cover the top portion of the array
 		
-		for (int i=0; i<matrix.length; i++) {
-			for (int j=0; j<matrix[0].length ; j++)  {
+		for (int i=0; i<matrix.length; i++) { //loops through row
+			for (int j=0; j<matrix[0].length ; j++)  { //loops through column
+				//adds | between all entries in a row
 				System.out.print("|");
-				System.out.print(matrix[i][j]);
+				System.out.print(matrix[i][j]); //surround the value with '|'
 				System.out.print("|");
 			} //end for j
-			System.out.println("\n"+returnDashes(matrix[0].length * 4));
+			System.out.println("\n"+returnDashes(matrix[0].length * 4)); // prints string line of dashes to cover the bottom portion of each row and seperates rows
 		} // end for i
 	}
 	
-	
+	/* method getColumnCount
+	 * This method returns the number of columns within matrix
+	 * Parameters:
+	 * 		none
+	 * Return Value
+	 * 		none
+	 * Local Variables:
+	 *	 	none
+	 */
 	public int getColumnCount() {
-		return matrix[0].length;	
+		return matrix[0].length;	//return the length of row number 1
 	}
+	/* method getRowCount
+	 * This method returns the number of rows within matrix
+	 * Parameters:
+	 * 		none
+	 * Return Value
+	 * 		none
+	 * Local Variables:
+	 *	 	none
+	 */
 	public int getRowCount() {
-		return matrix.length;
+		return matrix.length;//return the length of a column
 	}
 	
-	
+	/* method getRow
+	 * This method returns a specific row
+	 * Parameters:
+	 * 		value : int variable that represents the row number
+	 * Return Value
+	 * 		int [] variable that represents a row
+	 * Local Variables:
+	 *	 	none
+	 */
 	public int[] getRow(int value) {
-		return matrix[value];
+		return matrix[value]; //returns a row at position value
 	}
+	/* method getColumn
+	 * This method returns a specific column
+	 * Parameters:
+	 * 		value : int variable that represents the column number
+	 * Return Value
+	 * 		tempArray : int [] variable that represents a column
+	 * Local Variables:
+	 * 		tempArray : int [] variable that represents a column
+	 */
 	public int[] getColumn(int value) {
-		int [] tempArray;
-		tempArray = new int [getRowCount()];
-		for (int i =0; i<getRowCount();i++)
-			tempArray[i] = matrix[i][value];
+		int [] tempArray; //initializes the tempArray var
+		tempArray = new int [getRowCount()]; // sets the length of tempArray to the number of rows
+		for (int i =0; i<getRowCount();i++) //loops through each entry in the column
+			tempArray[i] = matrix[i][value]; //sets the indiviual values wihin the column
 		
-		return tempArray;		
+		return tempArray;	//returns the array	
 	}
 	
+	/* method getTranspose
+	 * This method returns the transpose of matrix
+	 * Parameters:
+	 * 		none
+	 * Return Value
+	 * 		source : Matrix variable that is the transpose of matrix
+	 * Local Variables:
+	 * 		source : Matrix variable that is the transpose of matrix
+	 */
 	public Matrix getTranspose() {
-		Matrix source = new Matrix(getColumnCount(), getRowCount());
-		for (int i=0; i<matrix.length;i++) {
+		Matrix source;//initializes source
+		source = new Matrix(getColumnCount(), getRowCount()); //calls constructor and initializes it to the opposite dimensions of matrix
+		for (int i=0; i<matrix.length;i++) { //loops through matrix
 			for (int j =0; j<matrix[0].length;j++) {
-				source.matrix[j][i] = matrix[i][j];
+				source.matrix[j][i] = matrix[i][j];//assigns the values of source by using the opposite column and row numbers
 			}
 		}
-		return source;
+		return source; //returns the new Matrix of the transpose
 	}
 	
+	/* method getEquals
+	 * This method returns if matrix is equal to another Matrix object
+	 * Parameters:
+	 * 		obj : Matrix variable that represents a new object/instance of Matrix
+	 * Return Value
+	 * 		valid : boolean variable that represents if the two Matrix's are equal
+	 * Local Variables:
+	 * 		valid : boolean variable that represents if the two Matrix's are equal
+	 */
 	public boolean getEquals(Matrix obj) { 
-		boolean valid;
-		valid = false;
+		boolean valid; //initializes valid
+		valid = true; //sets value to false 
 		
-		if (obj.getRowCount() == getRowCount() && obj.getColumnCount() == getColumnCount()) {
+		if (obj.getRowCount() == getRowCount() && obj.getColumnCount() == getColumnCount()) { //checks if the dimensions of both Matrix instances are the same
 				//verefied same length of matrix total
-				for (int i=0; i<matrix.length;i++) {
+				for (int i=0; i<matrix.length;i++) { //loops through matrix
 					for (int j =0; j<matrix[0].length;j++) {
-						if(matrix[i][j] == obj.getMatrix()[i][j]) {
-							valid = true;
+						if(matrix[i][j] != obj.getMatrix()[i][j]) { //checks if each value is also the same
+							valid = false; //if it is not the same then it changes the valif to false
 						}
-						else
-							valid = false;
 
 					}
 					
 				}//for  loop
 		}
 
-		return valid;
+		return valid; //returns the valid variable which if the instances are the EXACT same
 	}
+	
+	/* method getMatrix
+	 * This method returns matrix
+	 * Parameters:
+	 * 		none
+	 * Return Value
+	 * 		int [][] variable that contains the matrix
+	 * Local Variables:
+	 * 		none
+	 */
 	public int [][] getMatrix(){
-		return matrix;
+		return matrix;//returns matrix
 	}
 }
 

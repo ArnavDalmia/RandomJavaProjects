e3ed672b01a38ff5e46fc6f137fceacbdce2aabe
Arnav Dalmia <70185864+ArnavDalmia@users.noreply.github.com>
2022-12-07 19:41:24 -0500

    2 dimenssion version added

--- leafAssmt/src/Main.java
+++ leafAssmt/src/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		ArrayList <Integer> numbers = new ArrayList<>();
-		ArrayList <Integer> stem = new ArrayList<>();
-		ArrayList <String> leaf = new ArrayList<>();
+        ArrayList <ArrayList <Integer> > graph = new ArrayList<>();
+
 
 		int firstInput = Utils.obtainIntegerInputLow("Enter how many numbers you would like: ", 1);
 		int secondInput = Utils.obtainIntegerInputLowHigh("Enter low bound for range of numbers: ", 10, 999);
@@ -18,28 +18,45 @@ public class Main {
 
 		numbers.sort(null);
 
-		int highest;
-		highest  = numbers.get(numbers.size()-1);
-		for(int i = 1; i<= highest/10; i++){
-			stem.add(i);
+            // 12,23,34,35
+
+		int highestStem;
+		highestStem  = numbers.get(numbers.size()-1);
+        highestStem = highestStem/10;
+		for(int i = 0; i< highestStem; i++){
+			graph.add(new ArrayList<Integer>());
+            graph.get(i).add(i+1);
 		}
 
-		for(int i=1; i < stem.size() + 1;i++){
-			String tempString = "";
+        for(int i=1; i < graph.size() + 1;i++){
 			for(int j=0; j < firstInput;j++){
 				int number;
 				number  =  numbers.get(j) - (i*10);
 				if (number < 10 && number > -1)
-					tempString += (number + " ");
+                    graph.get(i-1).add(number);
 					
 			}
-			leaf.add(tempString);
 		}
 
-		for(int i=0; i < stem.size();i++){
-		System.out.println(stem.get(i) + " : " + leaf.get(i) + "\n");
-		} 
-	
+        System.out.println(numbers);
+        String header = String.format("%-10s %s", "Stem", "Leaf");
+        System.out.println(header);
+
+        for (int i=0; i<graph.size();i++) {
+            String temp;
+            temp = "";
+			for (int j=0; j<graph.get(i).size();j++){
+                if(j == 0)
+                    temp += String.format("%s", graph.get(i).get(j));
+                else{
+                    temp += String.format("%-10s", graph.get(i).get(j)); 
+                    
+                }
+            }
+            System.out.println(temp);
+		}
+        
+
 	}
 
 }
